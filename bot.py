1c65c2151dc02b68adc912aed1a151c6c6dbe429
ironjanowar <ironjanowar@gmail.com>
2016-04-04 22:02:58 +0200

    Minor changes

--- bot.py
+++ bot.py
@@ -15,9 +15,9 @@ with open('./convertor.token', 'r') as TOKEN:
 commands = {
     'start': 'Starts the bot!',
     'help': 'Show commands abailables',
-    'todec': 'Takes an hexadecimal or binary number and returns his decimal equivalent',
-    'tohex': 'Takes a decimal number and returns his hexadecimal equivalent',
-    'tobin': 'Takes a decimal number and returns his binary equivalent'
+    'todec': 'Takes an hexadecimal or binary number and returns its decimal equivalent',
+    'tohex': 'Takes a decimal number and returns its hexadecimal equivalent',
+    'tobin': 'Takes a decimal number and returns its binary equivalent'
 }
 
 # Handlers
